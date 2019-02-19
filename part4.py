e21a4c784bda48e58357aba55db4a26a082b69f9
alexisbdr <alexis.baudron15@gmail.com>
2019-02-19 02:38:42 -0600

    part4

--- part4.py
+++ part4.py
@@ -10,7 +10,6 @@ class DNS_proxy:
 	port = 53
 	host = socket.gethostbyname(socket.gethostname())
 	CHUNK_SIZE = 4096
-	DNS_QUERY_MESSAGE_HEADER = Struct("!3H")
 	DNS_IP = '8.8.8.8' #Google IP
 
 	def __init__(self):
@@ -27,8 +26,6 @@ class DNS_proxy:
 			self.tcp_sock.setblocking(0)
 
 			self.sockets = [self.tcp_sock, self.udp_sock]
-
-			print(self.host)
 
 		except (Exception) as e:
 			self.shutdown_with_error(str(e))
@@ -50,55 +47,62 @@ class DNS_proxy:
 
 
 	def no_such_name(self, message, rcode_index):
-		response = self.DNS_QUERY_MESSAGE_HEADER.unpack_from(message)
-		rcode_digit = response[rcode_index]
-		rcode_digit_hex = hex(rcode_digit)[-1]
-
-		response2 = None
-		counter = 4
-		fmt = "!4H"
+		success_lst = [11008, 33152, 1, 1, 0, 0, 887, 30583, 1639, 28527, 26476, 25859, 25455, 27904, 1, 1, 49164, 1, 1, 0, 299, 4, 44249, 1092]
+		response = None
+		counter = 3
+		fmt = "!3H"
 		while True:
 			try:
-				response2 = unpack_from(fmt, message)
+				response = unpack_from(fmt, message)
 				counter += 1
 				fmt = "!" + str(counter) + "H"
 			except:
 				break
 
-		print(response2)
+		rcode_digit = response[rcode_index]
+		rcode_digit_hex = hex(rcode_digit)[-1]
+
 		host_split = str(self.host).split('.')
 		host_hexSplit = []
-		host_hex_string = ""
 		for numberIndex in range(len(host_split)):
 			number = host_split[numberIndex]
 			ans = hex(int(number))
 			host_hexSplit.append(ans)
-			if len(ans) == 3:
-				host_hex_string += "0" + ans[2] # add spaces between entries
-				# something
-			elif len(ans) == 4:
-				host_hex_string += ans[2:]
-			if numberIndex < len(host_split) - 1:
-				host_hex_string += " "
 
 		if rcode_digit_hex == "0":
 			return message
 		elif rcode_digit_hex == "3":
-			print("enter 3 digits hex")
-			response2_lst = list(response2)
-			response2_lst[rcode_index] = 33152
+			response_lst = list(response)
+			if rcode_index == 2:
+				response_lst = response_lst[1:]
 			if len(host_hexSplit) == 4:
 				first = int(str(host_hexSplit[0][2:] + host_hexSplit[1][2:]), 16)
 				second = int(str(host_hexSplit[2][2:] + host_hexSplit[3][2:]), 16)
-				response2_lst[-2] = first
-				response2_lst[-1] = second
+				response_lst[-2] = first
+				response_lst[-1] = second
 
-			response2 = tuple(response2_lst)
+			prevEntry = response_lst[6]
+			query_lst = [prevEntry]
+			for entry in response_lst[7:]:
+				if entry == 0:
+					query_lst.append(1)
+					query_lst.append(1)
+					break
+				elif entry == 1 and prevEntry == 1:
+					query_lst.append(1)
+					break
+				query_lst.append(entry)
+				prevEntry = entry
+
+			final_response_lst = response_lst[0:1] + success_lst[1:6] + query_lst + success_lst[16:]
+			final_response_lst[-2] = response_lst[-2]
+			final_response_lst[-1] = response_lst[-1]
+			final_rsps_len = len(final_response_lst) * 2
+			final_response_lst = [final_rsps_len] + final_response_lst
+			final_response = tuple(final_response_lst)
 			new_ans = ""
-			for val in response2:
+			for val in final_response:
 				new_ans += pack("!H", val)
-
-			print(response2)
 			return new_ans
 
 	def handle_udp(self, sock):
